21d90508dbcca93ea6e500eea3e29f2df8c453da
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-28 12:39:57 +0530

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Workers from "./Workers";
 
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+                <div className="content">
+                    <Switch>
+                        <Route exact path="/">
+                            <Workers/>
+                        </Route>
+                        {/*<Route  path="/create">*/}
+                        {/*    <Create />*/}
+                        {/*</Route>*/}
+                        {/*<Route path="/blogs/:id">*/}
+                        {/*    <BlogDetails />*/}
+                        {/*</Route>*/}
+                        {/*<Route path="*">*/}
+                        {/*    <NotFound />*/}
+                        {/*</Route>*/}
+                    </Switch>
+                </div>
+                <Footer/>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ frontend/src/Workers.js
@@ -0,0 +1,55 @@
+import { Link } from "react-router-dom";
+
+
+const Workers = () => {
+    return (
+        <section className="ftco-section ftco-team">
+        <div className="container-fluid px-md-5">
+            <div className="row justify-content-center pb-3">
+                <div className="col-md-10 heading-section text-center ftco-animate">
+                    <span className="subheading">Artistic Director</span>
+                    <h2 className="mb-4">Makeup Artist</h2>
+                    <p>This is our professional workers</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-12 ftco-animate">
+                    <div className="carousel-team owl-carousel">
+                        <div className="item">
+                            <a href="#" className="team text-center">
+                                <div className="img" style="background-image: url(234.jpg);"></div>
+                                <h2>chandani Senewirathna</h2>
+                                <span className="position">Hair Stylist</span>
+                            </a>
+                        </div>
+                        <div className="item">
+                            <a href="#" className="team text-center">
+                                <div className="img" style="background-image: url(mi.jpg);"></div>
+                                <h2>Nishani Kulathunga</h2>
+                                <span className="position">Nail Master</span>
+                            </a>
+                        </div>
+                        <div className="item">
+                            <a href="#" className="team text-center">
+                                <div className="img" style="background-image: url(me.jpg);"></div>
+                                <h2>Raman Jayasooriya</h2>
+                                <span className="position">Director</span>
+                            </a>
+                        </div>
+                        <div className="item">
+                            <a href="#" className="team text-center">
+                                <div className="img" style="background-image: url(256.jpg);"></div>
+                                <h2>Nayani Fonseka</h2>
+                                <span className="position">Hair Stylist</span>
+                            </a>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    );
+}
+
+export default Workers;
