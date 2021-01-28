e5efe7937f09059f94e63622153fe3d1c37e32d8
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-28 21:53:28 +0530

    gallery

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -18,40 +18,37 @@ import {
 } from 'reactstrap';
 
 import img6 from './img5.jpg';
+import {useState} from "react";
 
 const Cards = () => {
+    const [title, setTitle] = useState('Title');
     return (
         <CardDeck>
             <Card>
                 <CardImg top width="100%" src={img6} alt="Card image cap" />
                 <CardImgOverlay>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                    <CardText>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </CardText>
+                    <CardTitle>{title}</CardTitle>
                 </CardImgOverlay>
 
             </Card>
             <Card>
                 <CardImg top width="100%" src={img6} alt="Card image cap" />
                 <CardImgOverlay>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                    <CardText>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </CardText>
+                    <CardTitle>{title}</CardTitle>
                 </CardImgOverlay>
 
             </Card>
             <Card>
                 <CardImg top width="100%" src={img6} alt="Card image cap" />
                 <CardImgOverlay>
-                    <CardTitle>Card Title</CardTitle>
-                    <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-                    <CardText>
-                        <small className="text-muted">Last updated 3 mins ago</small>
-                    </CardText>
+                    <CardTitle>{title}</CardTitle>
+                </CardImgOverlay>
+
+            </Card>
+            <Card>
+                <CardImg top width="100%" src={img6} alt="Card image cap" />
+                <CardImgOverlay>
+                    <CardTitle>{title}</CardTitle>
                 </CardImgOverlay>
 
             </Card>
