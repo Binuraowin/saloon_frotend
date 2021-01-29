57c1c830c30686d05ebf5bda01d0e24ef4401919
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-29 11:24:31 +0530

    workers

--- frontend/src/Workers.js
+++ frontend/src/Workers.js
@@ -18,80 +18,53 @@ import {
     Col
 } from 'reactstrap';
 import img6 from './img5.jpg';
+import {useState} from "react";
 
 const Workers = () => {
+    const [title, setTitle] = useState('Title');
+    const [subTitle, setSubTitle] = useState('Title');
     return (
 
-        <Row>
-            <Col xs="12" md="4">
-                {/* --------------------------------------------------------------------------------*/}
-                {/* Card-1*/}
-                {/* --------------------------------------------------------------------------------*/}
-                <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                    </CardBody>
-                    <img width="100%" src={img6} alt="" />
-                    <CardBody>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                        <CardLink href="#">Card Link</CardLink>
-                        <CardLink href="#">Another Link</CardLink>
-                    </CardBody>
-                </Card>
-            </Col>
-            <Col xs="12" md="4">
-                {/* --------------------------------------------------------------------------------*/}
-                {/* Card-1*/}
-                {/* --------------------------------------------------------------------------------*/}
-                <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                    </CardBody>
-                    <img width="100%" src={img6} alt="" />
-                    <CardBody>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                        <CardLink href="#">Card Link</CardLink>
-                        <CardLink href="#">Another Link</CardLink>
-                    </CardBody>
-                </Card>
-            </Col>
-            <Col xs="12" md="4">
-                {/* --------------------------------------------------------------------------------*/}
-                {/* Card-1*/}
-                {/* --------------------------------------------------------------------------------*/}
-                <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                    </CardBody>
-                    <img width="100%" src={img6} alt="" />
-                    <CardBody>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                        <CardLink href="#">Card Link</CardLink>
-                        <CardLink href="#">Another Link</CardLink>
-                    </CardBody>
-                </Card>
-            </Col>
-            <Col xs="12" md="4">
-                {/* --------------------------------------------------------------------------------*/}
-                {/* Card-1*/}
-                {/* --------------------------------------------------------------------------------*/}
-                <Card>
-                    <CardBody>
-                        <CardTitle>Card title</CardTitle>
-                        <CardSubtitle>Card subtitle</CardSubtitle>
-                    </CardBody>
-                    <img width="100%" src={img6} alt="" />
-                    <CardBody>
-                        <CardText>Some quick example text to build on the card title and make up the bulk of the cards content.</CardText>
-                        <CardLink href="#">Card Link</CardLink>
-                        <CardLink href="#">Another Link</CardLink>
-                    </CardBody>
-                </Card>
-            </Col>
-        </Row>
+<div>
+    <div>
+        <div className="artists-header">
+            <span >Artistic Director</span>
+            <h2 >Makeup Artist</h2>
+            <p>This is our professional workers</p>
+        </div>
+    </div>
+    <CardDeck>
+        <Card>
+            <CardImg top width="100%" src={img6} alt="Card image cap" />
+            <CardBody>
+                <CardTitle>{title}</CardTitle>
+                <CardSubtitle> {subTitle}</CardSubtitle>
+            </CardBody>
+        </Card>
+        <Card>
+            <CardImg top width="100%" src={img6} alt="Card image cap" />
+            <CardBody>
+                <CardTitle>{title}</CardTitle>
+                <CardSubtitle> {subTitle}</CardSubtitle>
+            </CardBody>
+        </Card>
+        <Card>
+            <CardImg top width="100%" src={img6} alt="Card image cap" />
+            <CardBody>
+                <CardTitle>{title}</CardTitle>
+                <CardSubtitle> {subTitle}</CardSubtitle>
+            </CardBody>
+        </Card>
+        <Card>
+            <CardImg top width="100%" src={img6} alt="Card image cap" />
+            <CardBody>
+                <CardTitle>{title}</CardTitle>
+                <CardSubtitle> {subTitle}</CardSubtitle>
+            </CardBody>
+        </Card>
+    </CardDeck>
+</div>
+
 
     );
 }
