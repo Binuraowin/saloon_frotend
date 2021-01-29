04b7a23e9ecf8ea1a459c9bc4a855e1605befc35
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-29 22:44:36 +0530

    service

--- frontend/src/ServiceList.js
+++ frontend/src/ServiceList.js
@@ -19,10 +19,11 @@ import {
 } from 'reactstrap';
 
 import img6 from './beard.png';
+import Appointment from "./Appointement";
 const ServiceList = () => {
     return (
 
-            <div >
+            <div className="service-my">
                 <div>
                     <div className="artists-header">
                         <span >Services</span>
@@ -30,41 +31,48 @@ const ServiceList = () => {
                         <p>This is our professional services we gives to you</p>
                     </div>
                 </div>
-                <CardDeck>
-                    <Card>
-                        <CardImg top width="100%" src={img6} alt="Card image cap" />
-                        <CardBody className="card-body-my">
-                            <CardTitle className="card-body-my">Makeup</CardTitle>
-                            <CardText>We Are professional for your Makeup </CardText>
+                <div>
+                    <CardDeck>
+                        <Card>
+                            <CardImg top width="100%" src={img6} alt="Card image cap" />
+                            <CardBody className="card-body-my">
+                                <CardTitle className="card-body-my">Makeup</CardTitle>
+                                <CardText>We Are professional for your Makeup </CardText>
 
-                        </CardBody>
-                    </Card>
-                    <Card>
-                        <CardImg top width="100%" src={img6} alt="Card image cap" />
-                        <CardBody className="card-body-my">
-                            <CardTitle>Beard</CardTitle>
-                            <CardText>We Are professional for your Bread </CardText>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg top width="100%" src={img6} alt="Card image cap" />
+                            <CardBody className="card-body-my">
+                                <CardTitle>Beard</CardTitle>
+                                <CardText>We Are professional for your Bread </CardText>
 
-                        </CardBody>
-                    </Card>
+                            </CardBody>
+                        </Card>
 
-                    <Card>
-                        <CardImg top width="100%" src={img6} alt="Card image cap" />
-                        <CardBody className="card-body-my">
-                            <CardTitle>Haircut &amp; Styling</CardTitle>
-                            <CardText>We Are professional for Haircut and Styling</CardText>
+                        <Card>
+                            <CardImg top width="100%" src={img6} alt="Card image cap" />
+                            <CardBody className="card-body-my">
+                                <CardTitle>Haircut &amp; Styling</CardTitle>
+                                <CardText>We Are professional for Haircut and Styling</CardText>
 
-                        </CardBody>
-                    </Card>
-                    <Card>
-                        <CardImg top width="100%" src={img6} alt="Card image cap" />
-                        <CardBody className="card-body-my">
-                            <CardTitle>Body Treatment</CardTitle>
-                            <CardText>We Are professional for your Body Treatment</CardText>
+                            </CardBody>
+                        </Card>
+                        <Card>
+                            <CardImg top width="100%" src={img6} alt="Card image cap" />
+                            <CardBody className="card-body-my">
+                                <CardTitle>Body Treatment</CardTitle>
+                                <CardText>We Are professional for your Body Treatment</CardText>
 
-                        </CardBody>
-                    </Card>
-                </CardDeck>
+                            </CardBody>
+                        </Card>
+                    </CardDeck>
+                </div>
+                <br/>
+                <div className="align-content-between">
+                    <Appointment/>
+                </div>
+
             </div>
 
     );
