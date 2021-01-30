2aaad63365b2b8552f994c9c4e57fc2720dcc13e
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-30 12:18:34 +0530

    basic-ui

--- frontend/src/Appointement.js
+++ frontend/src/Appointement.js
@@ -4,14 +4,18 @@ import { useHistory } from "react-router-dom";
 
 const Appointment = () => {
     const [title, setTitle] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
     const [body, setBody] = useState('');
     const [date, setDate] = useState('');
-    const [author, setAuthor] = useState('binura');
+    const [phone, setPhone] = useState('');
+    const [artists, setArtists] = useState('binura');
+    const [time, setTime] = useState('8.30am-9.30am');
     const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const blog = { title, body, author };
+        const blog = { title, body, artists };
 
         fetch('http://localhost:8000/blogs/', {
             method: 'POST',
@@ -27,14 +31,56 @@ const Appointment = () => {
         <div className="create">
             <h2>Make an Appointment</h2>
             <form onSubmit={handleSubmit}>
-                <label>Date:</label>
-                <input
-                    className="bg-light p-2 border"
-                    type="date"
-                    required
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                />
+
+                <Row className="mt-3">
+                    <Col xs="6">
+                        <label>Name:</label>
+                        <input
+                            className="bg-light p-2 border"
+                            type="text"
+                            required
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        {/*<div className="bg-light p-2 border">.col-6</div>*/}
+                    </Col>
+                    <Col xs="6">
+                        <label>Email:</label>
+                        <input
+                            className="bg-light p-2 border"
+                            type="email"
+                            required
+                            value={date}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        {/*<div className="bg-light p-2 border">.col-6</div>*/}
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Col xs="6">
+                        <label>Date:</label>
+                        <input
+                            className="bg-light p-2 border"
+                            type="date"
+                            required
+                            value={date}
+                            onChange={(e) => setDate(e.target.value)}
+                        />
+                        {/*<div className="bg-light p-2 border">.col-6</div>*/}
+                    </Col>
+                    <Col xs="6">
+                        <label>Phone:</label>
+                        <input
+                            className="bg-light p-2 border"
+                            type="text"
+                            required
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        />
+                        {/*<div className="bg-light p-2 border">.col-6</div>*/}
+                    </Col>
+                </Row>
+
                 <label>Description:</label>
                 <textarea
                     className="bg-light p-2 border"
@@ -42,23 +88,35 @@ const Appointment = () => {
                     value={body}
                     onChange={(e) => setBody(e.target.value)}
                 />
-                <label>Consumer:</label>
-                <select
-                    className="bg-light p-2 border"
-                    value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
-                >
-                    <option value="binura">binura</option>
-                    <option value="owin">owin</option>
-                </select>
-                <select
-                    className="bg-light p-2 border"
-                    value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
-                >
-                    <option value="binura">binura</option>
-                    <option value="owin">owin</option>
-                </select>
+                <Row className="mt-3">
+                    <Col xs="6">
+                        <label>Consumer:</label>
+                        <select
+                            className="bg-light p-2 border"
+                            value={artists}
+                            onChange={(e) => setArtists(e.target.value)}
+                        >
+                            <option value="binura">binura</option>
+                            <option value="owin">owin</option>
+                        </select>
+                    </Col>
+                    <Col xs="6">
+                        <label>Time:</label>
+                        <select
+                            className="bg-light p-2 border"
+                            value={time}
+                            onChange={(e) => setTime(e.target.value)}
+                        >
+                            <option value="1">8.30am-9.30am</option>
+                            <option value="2">9.30am-10.30am</option>
+                            <option value="3">10.30am-11.30am</option>
+                            <option value="4">12.30pm-01.30pm</option>
+                        </select>
+                    </Col>
+                </Row>
+
+
+
                 <button>Make an Appointment</button>
             </form>
         </div>
